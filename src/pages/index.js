bf1e70fad7d5803ff1eedd9a18da3543516dc366
iter8 <randomdweller.me@gmail.com>
2023-04-22 16:25:07 +0530

    Simple Temp Landing Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,10 @@
 import Head from "next/head";
-import { UserContext } from "../utils/Contexts";
-import styles from "../styles/Landing.module.css";
-import React, { useContext } from "react";
-
-import { loginWithRedirect } from "../utils/Firebase";
+import React from "react";
+import Landing from "../views/Landing";
 
 export default function Home() {
-  const { setCurrentUser } = useContext(UserContext);
-
-  async function loginHandle() {
-    const { user } = await loginWithRedirect();
-    setCurrentUser(user);
-  }
-
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Brief Me</title>
         <meta name='description' content='Dashboard' />
@@ -23,18 +13,14 @@ export default function Home() {
         <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png'></link>
         <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png'></link>
         <link rel='manifest' href='/site.webmanifest'></link>
+        {/* Temp addn of fonts for landing page.  */}
+        <link
+          href='https://fonts.googleapis.com/css2?family=Mulish:wght@400;700&family=Open+Sans:wght@400;700&display=swap'
+          rel='stylesheet'
+        ></link>
       </Head>
 
-      <div className='btnContainer'>
-        <button
-          className='btn'
-          onClick={() => {
-            loginHandle();
-          }}
-        >
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt, eos! Login.
-        </button>
-      </div>
+      <Landing />
     </div>
   );
 }
